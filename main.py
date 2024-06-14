0a2691c47775f0c3a563585e4997d84fe22c5ac3
Federico Lucero <federico.lucero@itti.digital>
2024-06-14 11:54:13 -0300

    done with the exercise

--- main.py
+++ main.py
@@ -23,11 +23,23 @@ def get_char_count(text):
             char_dict[lowered] = 1
     return char_dict
 
+
+# En esta funcion primero me fijo que los caracteres sean alfabeticos. Si lo son, los agrego en un diccionario con su letra y cantidad.
+# Despues los ordeno de mayor a menor y los imprimo de acuerdo al formato. La funcion lambda es la encargada de detectar esos valores y como esta en el key del sort, se basa en eso para el ordenamiento.
+
+
 def print_report(char_dict, path, words_count):
+    char_list = []
     print(f"--- Begin report {path} ---")
     print(f"{words_count} words found in the document")
-    for key in char_dict:
-        print(f"The \'{key}\' character was found {key[0]} times")
+    for char in char_dict:
+        if char.isalpha():
+            char_list.append({"char":char, "count":char_dict[char]})
+    
+    char_list.sort(key=lambda x: x["count"], reverse=True)
+    for item in char_list:
+        print(f"The \'{item["char"]}\' character was found {item["count"]} times")
+
     print("--- End report ---")
         
         
